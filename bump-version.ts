2ebc1d0019035be2a6315c9338bc5e7db2837232
supawut.thm <supawut.thm@cpf.co.th>
2022-09-03 21:23:13 +0700

    build: fix bump-version

--- bump-version.ts
+++ bump-version.ts
@@ -1,5 +1,8 @@
 //setup @jscutlery/semver and commit as Conventional Commits before using this script
 import {execSync} from 'node:child_process';
+import dotenv from "dotenv";
+
+dotenv.config()
 
 let a = execSync('yarn nx print-affected --target=version --trackDeps --base=main~1').toString();
 // console.log('exec',a.split('Done in').length)
@@ -8,9 +11,25 @@ let obj2 = obj.substring(obj.indexOf('{'))
 // console.log('object',obj.substring(obj.indexOf('{')))
 const res = JSON.parse(obj2);
 console.log('exec res',res)
+// execSync(`${t.command}`)
+// execSync('bash test.sh')
 //@ts-ignore
 res.tasks.forEach(t => {
     console.log(t.command)
     execSync(`${t.command}`)
   // invokeOnRemote(t.command);
 });
+
+
+// "publish": {
+//   "executor": "@nrwl/workspace:run-commands",
+//   "options": {
+//     "command": "node tools/scripts/publish.mjs ddd {args.ver} {args.tag}"
+//   },
+//   "dependsOn": [
+//     {
+//       "projects": "self",
+//       "target": "build"
+//     }
+//   ]
+// },
