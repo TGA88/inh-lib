03f2ba0f7af515011bbf6ca362597ef32ae50b0a
best <supawut.thomas@gmail.com>
2025-01-24 10:08:57 +0700

    fix(common): update type inh-producer for SNS

--- packages/common/src/lib/type/communication/shared/inh-producer.ts
+++ packages/common/src/lib/type/communication/shared/inh-producer.ts
@@ -5,12 +5,11 @@ import { Result } from '../../../Result';
 import {InhMessageFormat} from './message.type'
 
 export interface InhPublishCommandItf<O,F> {
-     //Broadcast
-  execute: <I>(data: InhMessageFormat<I>,messageGroupId?:string) => Promise<Result<O,F>>;
+  //Broadcast
+execute: <I>(data: InhMessageFormat<I>,options?:InhPublishInputOptions) => Promise<Result<O,F>>;
 }
+export type InhPublishInputOptions = InhSnsPublishInputOptions
+export type InhSnsPublishInputOptions = {messageGroupId?: string,MessageDeduplicationId?:string}
 
-
-
-
-
-
+export type InhPublishCommandConfig = InhSnsPublishCommandConfig
+export type InhSnsPublishCommandConfig = {topicArn:string}
