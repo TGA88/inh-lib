f7fee3b539cc03d2070c05a18062d1b4d615851c
best <supawut.thomas@gmail.com>
2025-02-10 20:05:52 +0700

    fix(common): update FunctionChain to support generic type

--- packages/common/src/lib/FunctionChain.ts
+++ packages/common/src/lib/FunctionChain.ts
@@ -1,6 +1,8 @@
 import { Either, left, Result } from "./Result";
 import { createEventLogFormatFn, EventLogFormatter, InhLogContext, InhLogger } from "./type/inh-logger";
 
+export type FunctionChainState = Record<string, unknown>
+
 /**
  * Represents a function instance with an input and a function that processes the input.
  * @template I - The type of the input.
@@ -20,21 +22,46 @@ export type FunctionInstance<I> = {
  * @param data - The data to be processed by the step.
  * @returns An Either type representing the result of the step.
  */
-export type FunctionStep = {
-    (data?: unknown): Either<unknown, unknown>;
+export type FunctionStep<I, O> = {
+    (data: I): O
 };
 
 /**
  * Interface for the FunctionChain class.
  */
-export interface FunctionChainItf {
+/**
+ * Interface representing a chain of functions to be executed sequentially.
+ * 
+ * @template T - The type of the input data for the function chain.
+ * @template O - The type of the output result of the function chain.
+ * 
+ * @remarks
+ * The `FunctionChainItf` interface allows you to create a chain of functions
+ * that can be executed in sequence. Each function in the chain is referred to
+ * as a "step". You can add steps to the chain using the `addStep` method, 
+ * start the chain with initial data using the `start` method, and retrieve 
+ * the result of the chain using the `getResult` method.
+ * 
+ * @example
+ * ```typescript
+ * const chain: FunctionChainItf<number, string> = new FunctionChain<number, string>();
+ * chain.addStep(async (data) => data + 1)
+ *      .addStep(async (data) => data * 2)
+ *      .start(1)
+ *      .then(() => {
+ *          const result = chain.getResult();
+ *          console.log(result); // Output will depend on the implementation of getResult
+ *      });
+ * ```
+ */
+export interface FunctionChainItf<T, O> {
 
     /**
      * Adds a step to the function chain.
      * @param step - The step to be added.
      * @returns The FunctionChain instance.
      */
-    addStep(step: FunctionStep): FunctionChainItf;
+    addStep(step: FunctionStep<T, O>): FunctionChainItf<T, O>;
 
     totalSteps(): number;
 
@@ -43,33 +70,33 @@ export interface FunctionChainItf {
      * @param data - The initial data to start the chain.
      * @returns The FunctionChain instance.
      */
-    start(data: unknown): FunctionChainItf;
+    start(data: T): Promise<FunctionChainItf<T, O>>;
 
     /**
      * Retrieves the result of the function chain.
      * @returns An Either type representing the result of the function chain.
      */
-    getResult(): Either<unknown, unknown>;
+    getResult(): O
 }
 
 /**
  * Class representing a chain of functions to be executed sequentially.
  * Implements the FunctionChainItf interface.
  */
-export class FunctionChain implements FunctionChainItf {
+export class FunctionChain<T, O> implements FunctionChainItf<T, O> {
     private logger: InhLogger;
-    private logFmt:EventLogFormatter
-    private logCtx:InhLogContext
+    private logFmt: EventLogFormatter
+    private logCtx: InhLogContext
 
-    private steps: FunctionStep[] = [];
-    private currentResult: Either<unknown, unknown> | undefined;
+    private steps: FunctionStep<T, O>[] = [];
+    private currentResult: O | undefined;
 
-    constructor(originLogCtx: InhLogContext){
-        this.logCtx= originLogCtx.createChild()
-        this.logFmt = createEventLogFormatFn('FunctionChain',this.logCtx.context)
+    constructor(originLogCtx: InhLogContext) {
+        this.logCtx = originLogCtx.createChild()
+        this.logFmt = createEventLogFormatFn('FunctionChain', this.logCtx.context)
         this.logger = this.logCtx.logger;
     }
-    
+
     /**
      * Adds a step to the function chain.
      * @param step - The step to be added.
@@ -77,12 +104,14 @@ export class FunctionChain implements FunctionChainItf {
      * @remarks
      * This method appends the provided step to the internal list of steps.
      */
-    addStep(step: FunctionStep): FunctionChainItf {
+    addStep(step: FunctionStep<T, O>): FunctionChainItf<T, O> {
+        this.logger.debug(this.logFmt(`Add step [${step.name}] to the function chain`));
         this.steps.push(step);
         return this;
     }
 
     totalSteps(): number {
+        this.logger.debug(this.logFmt(`Get total steps in the function chain`,this.steps.length));
         return this.steps.length;
     }
 
@@ -95,38 +124,54 @@ export class FunctionChain implements FunctionChainItf {
      * through the first step and then sequentially through the remaining steps. If any step
      * results in a failure, the chain execution stops and the failure result is set.
      */
-    start(data: unknown): FunctionChainItf {
+    async start(data: T): Promise<FunctionChainItf<T, O>> {
         this.logger.info(this.logFmt("Begin function start"));
         if (this.steps.length === 0) {
             // If no steps are defined, set the current result to a failure
             this.logger.info(this.logFmt("No steps defined in the chain"));
-            this.currentResult = left(Result.fail("No steps defined in the chain"));
+            this.currentResult = left(Result.fail("No steps defined in the chain")) as O;
             return this;
         }
 
         // Execute the first step with the initial data
-        this.logger.info(this.logFmt(`Execute the first step [${this.steps[0].name}] with the initial data`, data));
+        this.logger.info(this.logFmt(`Execute the first step [${this.steps[0].name}] with the initial data`));
+        this.logger.debug(this.logFmt(`Initial data`, data));
 
-        let result = this.steps[0](data);
-        
+        let result = await this.steps[0](data) as Either<unknown, unknown>;
+        // If the result is a failure, break the loop
+        if (result.isLeft()) {
+            const fail = result.value;
+            this.logger.info(this.logFmt(`End function start with Error at step [${this.steps[0].name}]`));
+            this.logger.debug(this.logFmt(`Error`, fail));
+
+        }
+
         // Iterate through the remaining steps
         for (let i = 1; i < this.steps.length; i++) {
+            // previousState 
             const val = result.value;
+
+            // Execute the next step with the result of the previous step
+            const pvState = val as T;
+            const nextStep = this.steps[i];
+            this.logger.info(this.logFmt(`Execute the next step[${nextStep.name}] with the result of the previous step `));
+            this.logger.debug(this.logFmt(`Previous state`, pvState));
+
+            result = await nextStep(pvState) as Either<unknown, unknown>;
             // If the result is a failure, break the loop
             if (result.isLeft()) {
-                this.logger.info(this.logFmt(`End function start with Error at step [${this.steps[i].name}]`, val));
-                break;
+                this.logger.info(this.logFmt(`End function start with Error at step [${this.steps[i].name}]`));
+                this.logger.debug(this.logFmt(`step [${this.steps[i].name}] Error`, result.value));
+                this.currentResult = result as O;
+                return this
             }
-            // Execute the next step with the result of the previous step
-            const nextStep = this.steps[i];
-            
-            this.logger.info(this.logFmt(`Execute the next step[${nextStep.name}] with the result of the previous step `, val));
-            result = nextStep(val);
         }
 
         // Set the final result
-        this.logger.info(this.logFmt(`End function start successfully`,result));
-        this.currentResult = result;
+        this.logger.info(this.logFmt(`End function start successfully`));
+        this.logger.debug(this.logFmt(`Final state`, result.value));
+
+        this.currentResult = result as O;
         return this;
     }
 
@@ -137,42 +182,13 @@ export class FunctionChain implements FunctionChainItf {
      * This method returns the final result of the function chain execution. If the chain
      * has not been started, it returns a failure result.
      */
-    getResult(): Either<unknown, unknown> {
+    getResult(): O {
+        this.logger.info(this.logFmt(`Get result of the function chain`));
         if (this.currentResult === undefined) {
             // If the chain has not been started, return a failure
-            return left(Result.fail("Chain has not been started"));
+            this.logCtx.logger.error(this.logFmt(`Chain has not been started`));
+            return left(Result.fail("Chain has not been started")) as O;
         }
-        return this.currentResult;
+        return this.currentResult as O;
     }
 }
-
-
-
-
-
-// export class FunctionChain {
-//     private steps: FunctionStep[] = [];
-//     constructor(steps: FunctionStep[]){
-//         this.steps = steps;
-//     }
-//     public async start(data:unknown): Promise<Either<unknown,unknown>> {
-//         let isFirst=true;
-//         let result: Either<unknown,unknown> = this.steps[0](data);
-//         for (const step of this.steps) {
-//             if (isFirst) {
-//                 isFirst = false;
-    
-//                 result= step(data);
-//                 if (result.isLeft()) {
-//                     return result;
-//                 }
-//             }
-
-//             result = nextStep(step, result);
-//             if (result.isLeft()) {
-//                 break;
-//             }
-//         }
-//         return result;
-//     }
-// }
--- packages/common/src/lib/__test__/FunctionChain.test.ts
+++ packages/common/src/lib/__test__/FunctionChain.test.ts
@@ -1,57 +1,57 @@
 import { FunctionChain, FunctionStep } from '../FunctionChain';
-import {  left, right, Result } from '../Result';
+import { left, right, Result, Either } from '../Result';
 import { InhLogContext, InhLogger } from '../type/inh-logger';
 
 describe('FunctionChain', () => {
-  let functionChain: FunctionChain;
-let logContext: InhLogContext;
-  beforeEach(() => {
-  
-    logContext = new InhLogContext(console as unknown as InhLogger);
-    functionChain = new FunctionChain(logContext);
-  });
+    let functionChain: FunctionChain<number, Either<unknown, unknown>>;
+    let logContext: InhLogContext;
 
-  test('should add steps to the function chain', () => {
-    const step: FunctionStep = (data) => right(data);
-    functionChain.addStep(step);
-    expect(functionChain.totalSteps()).toBe(1);
-  });
+    beforeEach(() => {
+        logContext = new InhLogContext(console as unknown as InhLogger);
+        functionChain = new FunctionChain<number, Either<unknown, unknown>>(logContext);
+    });
 
-  test('should start the function chain and process data through steps', () => {
-    const step1: FunctionStep = (data) => right(data as number + 1);
-    const step2: FunctionStep = (data) => right(data as number * 2);
+    test('should add steps to the function chain', () => {
+        const step: FunctionStep<number, Either<unknown, unknown>> = (data) => right(data);
+        functionChain.addStep(step);
+        expect(functionChain.totalSteps()).toBe(1);
+    });
 
-    functionChain.addStep(step1).addStep(step2);
-    functionChain.start(1);
+    test('should start the function chain and process data through steps', async () => {
+        const step1: FunctionStep<number, Either<unknown, unknown>> = (data) => right(data + 1);
+        const step2: FunctionStep<number, Either<unknown, unknown>> = (data) => right(data * 2);
 
-    const result = functionChain.getResult();
-    expect(result.isRight()).toBe(true);
-    expect(result.value).toBe(4);
-  });
+        functionChain.addStep(step1).addStep(step2);
+        await functionChain.start(1);
 
-  test('should stop the chain execution if a step fails', () => {
-    const step1: FunctionStep = (data) => right(data as number + 1);
-    const step2: FunctionStep = () => left(Result.fail('Step 2 failed'));
-    const step3: FunctionStep = (data) => right(data as number * 2);
+        const result = functionChain.getResult();
+        expect(result.isRight()).toBe(true);
+        expect(result.value).toBe(4);
+    });
 
-    functionChain.addStep(step1).addStep(step2).addStep(step3);
-    functionChain.start(1);
+    test('should stop the chain execution if a step fails', async () => {
+        const step1: FunctionStep<number, Either<unknown, unknown>> = (data) => right(data + 1);
+        const step2: FunctionStep<number, Either<unknown, unknown>> = () => left(Result.fail('Step 2 failed'));
+        const step3: FunctionStep<number, Either<unknown, unknown>> = (data) => right(data * 2);
 
-    const result = functionChain.getResult();
-    expect(result.isLeft()).toBe(true);
-    expect(result.value).toEqual(Result.fail('Step 2 failed'));
-  });
+        functionChain.addStep(step1).addStep(step2).addStep(step3);
+        await functionChain.start(1);
 
-  test('should return failure if no steps are defined', () => {
-    functionChain.start(1);
-    const result = functionChain.getResult();
-    expect(result.isLeft()).toBe(true);
-    expect(result.value).toEqual(Result.fail('No steps defined in the chain'));
-  });
+        const result = functionChain.getResult();
+        expect(result.isLeft()).toBe(true);
+        expect(result.value).toEqual(Result.fail('Step 2 failed'));
+    });
 
-  test('should return failure if chain has not been started', () => {
-    const result = functionChain.getResult();
-    expect(result.isLeft()).toBe(true);
-    expect(result.value).toEqual(Result.fail('Chain has not been started'));
-  });
+    test('should return failure if no steps are defined', async () => {
+        await functionChain.start(1);
+        const result = functionChain.getResult();
+        expect(result.isLeft()).toBe(true);
+        expect(result.value).toEqual(Result.fail('No steps defined in the chain'));
+    });
+
+    test('should return failure if chain has not been started', () => {
+        const result = functionChain.getResult();
+        expect(result.isLeft()).toBe(true);
+        expect(result.value).toEqual(Result.fail('Chain has not been started'));
+    });
 });
