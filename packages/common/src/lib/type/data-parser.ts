fefa070abab2ecf7946d8506fb993c5f2c4286a5
best <supawut.thomas@gmail.com>
2025-11-02 20:46:18 +0700

    fix(common): type DataParser return both Result or Either type

--- packages/common/src/lib/type/data-parser.ts
+++ packages/common/src/lib/type/data-parser.ts
@@ -1,3 +1,4 @@
+import { Either } from "../Either";
 import { Result } from "../Result";
 
-export type DataParser<I, O,F=unknown> = (input: I) => Result<O,F>;
+export type DataParser<I, O,F=unknown> = (input: I) => Result<O,F> | Either<F,O>;
